bc95578c22ba234b57adad792bc9a1e63d4b7a7e
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-16 20:59:50 +0300

    image switching, asking and displaying user name

--- test_site/scripts/main.js
+++ test_site/scripts/main.js
@@ -1,2 +1,34 @@
-const heading = document.querySelector("h1");
-heading.textContent = "Hello World!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/cape.png") {
+    myImage.setAttribute("src", "images/image2.png");
+  } else {
+    myImage.setAttribute("src", "images/cape.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to my cool site, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome to my cool site, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
+  
